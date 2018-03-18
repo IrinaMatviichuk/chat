6de81d7135b0421aed0b47f5a7b4cb50f963c557
Irina Matviichuk <irina.matviichuk@gmail.com>
2018-03-18 21:05:04 +0200

    study project

---
+++ src/app/auth/auth.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'fp-auth',
+  templateUrl: './auth.component.html',
+  styleUrls: ['./auth.component.scss']
+})
+export class AuthComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
